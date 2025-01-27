f2382cf09e8307b1e9c15edaa8c6a6cd14d6715e
Brillyants <andrewbrillyant@gmail.com>
2025-01-27 11:46:43 +0700

    Add custom class to outlined button component

--- src/components/OutlinedButton.tsx
+++ src/components/OutlinedButton.tsx
@@ -1,11 +1,16 @@
 interface OutlinedButtonProps {
   text: string;
   url: string;
+  className?: string;
 }
 
-const OutlinedButton: React.FC<OutlinedButtonProps> = ({ text, url }) => {
+const OutlinedButton: React.FC<OutlinedButtonProps> = ({
+  text,
+  url,
+  className,
+}) => {
   return (
-    <div className="outlined-button">
+    <div className={`outlined-button ${className || ""}`}>
       <a href={url} target="_blank">
         <button>{text}</button>
       </a>
